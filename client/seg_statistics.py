6d17e356290b2483ff466fa7121ee31253751f03
fomy <root@b65-System-Product-Name.(none)>
2015-03-19 17:17:50 +0800

    modify the seg_statistics.py

--- client/seg_statistics.py
+++ client/seg_statistics.py
@@ -4,43 +4,58 @@ import sys
 import linecache
 import re
 
+#useage
+#./seg_static path version_num
+
 #The version number of statistics
 version_number = 10
+path = ""
 
-if len(sys.argv) > 2:
+if len(sys.argv) != 3:
     print 'input error'
 else:
-    if len(sys.argv) == 2:
-        version_number = int(sys.argv[1])
+    if len(sys.argv) == 3:
+        version_number = int(sys.argv[2])
+        path = str(sys.argv[1])
 
 #save the segments
 segments = []
 
 #Statistics the lines of file
 count = -1
-for count, line in enumerate(open("./tmp/static_output.txt", 'rU')):
-    pass
-count += 1
+for count, line in enumerate(open(path, 'rU')):
+    #pass
+    count += 1
+
+print count
 
 if version_number > count:
     version_number = count
 
-dict = {}
 #Reading the specified rows from the last row
-i = 1
-while i <= version_number:
-    line = linecache.getline('./tmp/static_output.txt', count - i + 1)
-    line = line[:-1]
-    list = line.split('/')
-    for item in list:
-    	info = item.split(',')
-    	if item[0] not in dict:
-    		dict[item[0]] = item[1]
+
+cur_count = version_number
+while cur_count <= count:
+
+    i = 1
+    dict = {}
+    while i <= version_number:
+        line = linecache.getline(path, cur_count - i + 1)
+        line = line[:-1]
+        list = line.split('/')
+    # print list
+        for item in list:
+    	    info = item.split(',')
+            # print info
+    	    if info[0] not in dict:
+    		    dict[info[0]] = info[1]
     
     		
       #  if segment not in segments:
        #     segments.append(segment)
-    i += 1
+        i += 1
     
-print ('The last %d version used %d segments' % (version_number, len(segments)))
-print segments
+    print ('The last %d version used %d segments' % (version_number, len(dict)))
+       # print dict
+
+    cur_count += 1
